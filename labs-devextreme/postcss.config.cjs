655e3e6005aa1943e994948e6365caefa1bbd76b
gyuannn <gyuannn1100@gmail.com>
2023-07-26 02:22:55 +0800

    chore: set different postcss config when dev / prod

--- labs-devextreme/postcss.config.cjs
+++ labs-devextreme/postcss.config.cjs
@@ -1,21 +1,29 @@
 /* eslint-disable */
 
-const purgecss = require('@fullhuman/postcss-purgecss')
 const prefixer = require('postcss-prefix-selector')
 
-module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    require('tailwindcss'),
-    // help replace devextreme classname `dx-` to `labs-` in .css file
-    prefixer({
-      prefix: '',
-      transform: function (prefix, selector) {
-        if (selector.includes('dx-')) {
-          return selector.replaceAll('dx-', 'labs-')
+module.exports = (ctx) => {
+  // development
+  if (ctx.env === 'development') {
+    return {
+      plugins: [require('autoprefixer'), require('tailwindcss')]
+    }
+  }
+  // production
+  return {
+    plugins: [
+      require('autoprefixer'),
+      require('tailwindcss'),
+      // help replace devextreme classname `dx-` to `labs-` in .css file
+      prefixer({
+        prefix: '',
+        transform: function (prefix, selector) {
+          if (selector.includes('dx-')) {
+            return selector.replaceAll('dx-', 'labs-')
+          }
+          return selector
         }
-        return selector
-      }
-    })
-  ]
+      })
+    ]
+  }
 }
