6414f2724fdcfc3a23510ca8ace20f56a76bd986
gyuannn <gyuannn1100@gmail.com>
2023-07-25 19:49:51 +0800

    version: bump to 0.1.2

--- labs-devextreme/src/main.ts
+++ labs-devextreme/src/main.ts
@@ -3,11 +3,4 @@ import App from './App.vue'
 import './assets//dx.generic.custom-scheme.css'
 import './style.css'
 
-Object.defineProperty(document, 'getElementsByClassName', {
-  value: null,
-  writable: false
-})
-
-console.log(document.getElementsByClassName)
-
 createApp(App).mount('#app')
