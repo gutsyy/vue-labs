117797fefecad777bf5682031ff86be0da77147c
gyuannn <gyuannn1100@gmail.com>
2023-08-30 08:58:33 +0800

    fix: ssr noExternal

--- docs/markdowns/.vitepress/config.ts
+++ docs/markdowns/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   vite: {
     ssr: {
-      noExternal: ['@gutsyy/labs-devextreme', 'devextreme']
+      noExternal: ['devextreme']
     }
   },
   title: 'vue labs',
