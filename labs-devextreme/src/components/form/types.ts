96f8159ce18e2128c64230f564fae7527ad83374
gyuannn <gyuannn1100@gmail.com>
2023-09-01 20:53:28 +0800

    feat: 支持自定义表单确认取消按钮文案

--- labs-devextreme/src/components/form/types.ts
+++ labs-devextreme/src/components/form/types.ts
@@ -3,6 +3,8 @@ export type FormContainerProps = {
   cols?: '2' | '4' | '6' | '12'
   showCancelButton?: boolean
   showConfirmButton?: boolean
+  confirmButtonName?: string
+  cancelButtonName?: string
   onConfirm?: () => void
   confirmButtonClassName?: string
   cancelButtonClassName?: string
